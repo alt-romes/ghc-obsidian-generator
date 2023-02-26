628456ddefb7f3ab2edafa5d60d336d3dec21189
romes <rodrigo.m.mesquita@gmail.com>
2023-02-26 01:10:52 +0000

    Break line comment notes

--- app/NewParser.hs
+++ app/NewParser.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 module NewParser where
 
@@ -28,48 +29,72 @@ data NoteTitle = NoteTitle Text Bool -- ^ Is historic note?
 
 newtype NoteReference = NoteReference Text deriving Show
 
-parseCNote :: Parser Note
-parseCNote = noteParser id (string "*/")
+data NoteType = LineNote | BlockNote
+
+-- parseCNote :: Parser Note
+-- parseCNote = noteParser id (string "*/")
 
 parseHaskellNote :: Parser Note
-parseHaskellNote = lineNote <|> blockNote
-  where
-    lineNote = noteParser (string "--" *>) (notFollowedBy (string "--"))
-    blockNote = noteParser id (string "-}") -- TODO: Doesn't end at pragma end
+parseHaskellNote = noteParser LineNote <|> noteParser BlockNote
+  -- where
+  --   lineNote = noteParser (hspace *> string "--" *>) (notFollowedBy (string "--" *> hspace))
+  --   blockNote = noteParser id (string "-}") -- TODO: Doesn't end at pragma end
+
 
 -- Assumes no previous input, much like how whitespace lexing works
-noteParser :: (∀ a. Parser a -> Parser a) -> Parser end -> Parser Note
-noteParser prefixf end = do
-  noteName  <- noteTitle prefixf
-  noteLines <- manyTill (prefixf noteLine) (void end <|> lookAheadNoteTitle)
-  skipAnyTill (lookAheadNoteTitle)
+noteParser :: NoteType -> Parser Note
+noteParser nt = do
+  noteName  <- noteTitle nt
+  noteLines <- manyTill (noteLine nt) (endComment nt <|> lookAheadNoteTitle)
+  skipLinesTill lookAheadNoteTitle
   let refs = case parse parseReferences "References" (T.unwords noteLines) of
         Left e -> error $ errorBundlePretty e
         Right r -> r
   pure $ Note {title = noteName, body=noteLines, references=refs, subnotes=[]}
     where
-      lookAheadNoteTitle = lookAhead (void $ noteTitle id) <|> eof
+      lookAheadNoteTitle = lookAhead (void $ noteTitle LineNote <|> noteTitle BlockNote) <|> eof
 
-noteTitle :: (∀ a. Parser a -> Parser a) -> Parser NoteTitle
-noteTitle prefixf = try $ do
-  historic <- prefixf $ optional (string "Historic" *> string "al" *> hspace) -- there exist both "Historic" and "Historical" Notes. Same meaning, different syntax
-  ntitle   <- symbol "Note" *> (string "[" *> manyTill anySingle (string "]")) <* eol
-                <* prefixf (hspace <* some (char '~') <* eol)
+noteTitle :: NoteType -> Parser NoteTitle
+noteTitle nt = try $ do
+  (historic, ntitle) <- case nt of
+    LineNote -> do
+      historic <- symbol "--" *> pHistoric
+      ntitle   <- symbol "--" *> pTitle
+                    <* hspace <* symbol "--" <* pTilde
+      pure (historic, ntitle)
+    BlockNote -> do
+      historic <- optional (symbol "{-") *> pHistoric -- optional {- to account for notes starting on the comment block
+      ntitle   <- hspace *> pTitle <* pTilde
+      pure (historic, ntitle)
   pure $ NoteTitle (T.pack ntitle) (isJust historic)
+  where
+    -- there exist both "Historic" and "Historical" Notes. Same meaning, different syntax
+    pHistoric = optional (string "Historic" *> string "al" *> hspace)
+    pTitle = symbol "Note" *> string "[" *> manyTill anySingle (string "]") <* skipAnyTill eol
+    pTilde = some (char '~') <* eol
+
+endComment :: NoteType -> Parser ()
+endComment = \case
+  LineNote -> notFollowedBy (string "--")
+  BlockNote -> void $ string "-}"
 
 parseReferences :: Parser [NoteReference]
 parseReferences = skipAnyTill lookAheadNoteRef *> manyTill (noteReference <* skipAnyTill lookAheadNoteRef) eof
-  where
-      lookAheadNoteRef = lookAhead (void noteReference) <|> eof
+  where lookAheadNoteRef = lookAhead (void noteReference) <|> eof
 
 noteReference :: Parser NoteReference
 noteReference = NoteReference . T.pack . unwords <$> try (symbol "Note" *> (string "[" *> manyTill (manyTill anySingle (space1 <|> void (lookAhead (char ']')))) (string "]")))
 
-noteLine :: Parser Text
-noteLine = T.pack <$> manyTill anySingle (void eol <|> eof)
+noteLine :: NoteType -> Parser Text
+noteLine = fmap T.pack . \case
+  LineNote  -> symbol "--" *> manyTill anySingle (void eol <|> eof)
+  BlockNote -> manyTill anySingle (void (lookAhead $ string "-}") <|> void eol <|> eof)
 
 skipAnyTill :: Parser end -> Parser end
 skipAnyTill = skipManyTill anySingle
+
+skipLinesTill :: Parser end -> Parser end
+skipLinesTill = skipManyTill (skipAnyTill (void eol <|> eof))
 
 symbol :: Text -> Parser Text
 symbol s = string' s <* hspace
@@ -78,7 +103,7 @@ notesInModule :: FilePath -> IO [Note]
 notesInModule fp = do
   -- TODO: If ends with... c call, hs call ...
   content <- T.readFile fp
-  let topParser = skipAnyTill (eof <|> void (lookAhead parseHaskellNote)) *> manyTill parseHaskellNote eof
+  let topParser = skipLinesTill (eof <|> void (lookAhead parseHaskellNote)) *> manyTill parseHaskellNote eof
   case parse topParser fp content of
     Right ns -> pure ns
     Left e -> fail ("When parsing " <> fp <> " got \"" <> errorBundlePretty e <> "\"")
